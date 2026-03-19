6873062016fc23b1e1f5b1eb3df5df2564e8fac7
KNikhith <nikhithkotluri@gmail.com>
2026-03-19 20:54:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+function daysOfAYear(year){
+	if((year%400===0)||(year%4===0 && year%100!==0)){
+		return 366;
+	}
+	return 365;
+}
+let year=Number(prompt("Enter the Year"));
+alert(daysOfAYear(year))
